3ad15f6d2201e100553e0606cdd4c733e61ec625
Giovanni <cascafico@gmail.com>
2024-02-08 11:51:59 +0100

    Update profile.py

--- profile.py
+++ profile.py
@@ -2,25 +2,21 @@
 
 # aggiunge tag source=pippo
 add_source = False
-source = 'Comune-di-Milano'
+source = 'Comune-di-Venezia'
 
 # do not add unique reference IDs to OSM?
 
-# aggiunge tag ref:<dataset_id>=<id del Comune di Milano (IDMASTER)>
+# aggiunge tag ref:<dataset_id>=<id del Comune di Venezia>
 # True -> relying only on geometric matching every time
 no_dataset_id = True
-dataset_id = 'ds634'
+dataset_id = 'Strato03_GestioneViabilita_Indirizzi'
 
 # Overpass query to use when searching OSM for data
 #overpass_timeout = 120 default
 overpass_timeout = 300
-#query = [('amenity', 'fuel'),('waterway', 'fuel')] both conditions
-#query = [('amenity', 'fuel')],[('waterway', 'fuel')]  or condition
-#query = [('amenity', 'fuel'),('disused:amenity','fuel')]  namespace disused and abandoned are implicit
 #query = [('addr:housenumber','.*')] 
 #query = [('addr:housenumber','~.*')]  e se lettera e interno non hanno stesso case?
 #query = [('addr:street','~.*'),('addr:city','Milano')] non tutti hanno city valorizzato
-
 query = [('addr:housenumber','~.*')] 
 # do not comment: seems needed even if external --osm data
 
@@ -33,7 +29,7 @@ max_request_boxes = 32
 
 
 # tags to replace on matched OSM objects
-master_tags = ('addr:housenumber', 'addr:street')
+master_tags = ('addr:housenumber', 'addr:street', 'addr:place')
 #master_tags = ('addr:street')
 
 # delete_unmatched = True cancellerebbe anche i POI con indirizzo
@@ -43,7 +39,7 @@ tag_unmatched = { 'fixme':'Maybe addr is plate in wrong position: please check' 
 
 # max distance to search for a match in meters (default 100)
 # max_distance = 10 several housenumbers in polygon centriod unmatched... wider distance
-max_distance = 10
+max_distance = 5
 
 # max distance to consider dataset duplicates
 duplicate_distance = 2
